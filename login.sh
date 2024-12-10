8dd756f48815c3cb19e50f2643075f9ba6fee819
peacefulhat <123523813+Peacefulhat@users.noreply.github.com>
2024-12-10 18:45:40 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,5 +2,5 @@
 yay -S catppuccin-sddm-theme-mocha
 sudo cp ~/Wallpaper/1.jpg /usr/share/sddm/themes/catppuccin-mocha/backgrounds
 sudo mv /usr/share/sddm/themes/catppuccin-mocha/backgrounds/1.jpg wall.jpg
-echo "go to /usr/share/themes/catppuccin-mocha and edit theme.conf"
+echo "going to /usr/share/themes/catppuccin-mocha and editing theme.conf"
 sudo cat "[Theme]\nCurrent=catppuccin-mocha"> /etc/sddm.conf
