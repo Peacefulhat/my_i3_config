940f16af37c8d3af19a23de25d3748984ca214f6
Peacefulhat <peacefulhat@gmail.com>
2025-01-16 10:17:27 +0530

    updation of my_i3_config_with aur package support

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/zsh
 file=$1
+
 ./install_pkg.sh $file
 echo"Installing smex..."
 git clone https://github.com/nonsequitur/smex.git
+
+echo "Installing yay..."
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo "removing yay folder..."
+rm -rf yay
+
+echo "copying files data ..."
 mkdir ~/.emacs.d
 ./copy.sh
 ./ohmyzsh.sh
